8ab2caede630829117ef0497cac5aa5f9d859e03
Diavo1o <66171057+Diavo1o@users.noreply.github.com>
2021-02-14 22:58:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@ module.exports = {
   password: process.env.TEXTNOW_PASSWORD || "",
   recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
   message: process.env.TEXTNOW_MESSAGE || "autosend message",
-  cookies_str: "__cfduid=dfe5bcb63261378632aea052724c6208b1613313653;G_ENABLED_IDPS=google;connect.sid=s%3AKnpM3-s2nyOLbiHukb5Z3CnxX3nrVJD_.DuCX%2BFarFO7Wf2iXTFNq%2BBL0inUVFJ6lr1WVbSmbv1U;FirehoseSession-messaging=true;language=zh;PermissionPriming=1;puntCookie=true;UserDidVisitApp=true;XSRF-TOKEN=19404U2c-NywZwH33CHfOqfQ5LacUKJu-T40"
+  cookies_str: process.env.TEXTNOW_COOKIES
 };
